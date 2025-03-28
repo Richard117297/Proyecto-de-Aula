73ad1590c64be1e80ffe646b055bb53a4a8d9af3
Richard117297 <richardcarvajalb@gmail.com>
2025-03-28 12:01:33 -0500

    Clases_GestionRutas_2

--- ListaEnlazadaSimple_Rutas.cs
+++ ListaEnlazadaSimple_Rutas.cs
@@ -1,15 +1,100 @@
-internal class ListaEnlazadaSimple_Rutas {
+internal class ListaEnlazadaSimple_Rutas
+{
+
+    private NodoDeRuta cabeza;
+
+    public NodoDeRuta Cabeza { get => cabeza; set => cabeza = value; }
+
+    public ListaEnlazadaSimple_Rutas()
+    {
+
+        cabeza = null;
+    }
+
+    public void addRuta(Ruta ruta)
+    {
+
+        NodoDeRuta nuevaRuta = new NodoDeRuta(ruta);
+
+        if (cabeza == null)
+        {
+
+            cabeza = nuevaRuta;
+
+        }
+
+        NodoDeRuta actual = cabeza;
+
+        while (actual.Siguiente != null)
+        {
+
+            actual = actual.Siguiente;
+
+        }
+        actual.Siguiente = nuevaRuta;
+    }
 
 
+    public void mostrarRutas()
+    {
+
+        if (cabeza == null)
+        {
+
+            Console.WriteLine($"La lista de rutas esta vacia.");
+            return;
+        }
+
+        NodoDeRuta actual = cabeza;
+
+        while (actual != null)
+        {
+
+            Console.WriteLine($"Id de la ruta : {actual.Ruta.IdRuta} ");
+            Console.WriteLine($"Fecha hora de salida : {actual.Ruta.FechaHoraDeSalida} ");
+            Console.WriteLine($"Estacion de partida : {actual.Ruta.EstacionDePartida} ");
+            Console.WriteLine($"Fecha Hora de Llegada : {actual.Ruta.FechaHoraDeLlegada} ");
+            Console.WriteLine($"Estacion de llegada :  {actual.Ruta.EstacionDeLlegada} ");
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine();
+
+            actual = actual.Siguiente;
+        }
+    }
 
 
+    public void BuscarRuta(Ruta idRuta)
+    {
 
 
+        if (cabeza == null)
+        {
+            Console.WriteLine("La lista de rutas está vacía.");
+            return;
+        }
+
+        NodoDeRuta actual = cabeza;
+
+        while (actual != null)
+        {
+            if (actual.Ruta.IdRuta.Equals(idRuta))
+            {
+                Console.WriteLine($"La ruta econtrada tiene el Id : {actual.Ruta.IdRuta} ");
+                Console.WriteLine($"Fecha hora de salida : {actual.Ruta.FechaHoraDeSalida} ");
+                Console.WriteLine($"Estacion de partida : {actual.Ruta.EstacionDePartida} ");
+                Console.WriteLine($"Fecha Hora de Llegada : {actual.Ruta.FechaHoraDeLlegada} ");
+                Console.WriteLine($"Estacion de llegada :  {actual.Ruta.EstacionDeLlegada} ");
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine();
+                return;
+            }
+            actual = actual.Siguiente;
+        }
+
+        Console.WriteLine("No se encontró el boleto.");
 
 
-
-
-
+    }
 
 
 
--- Ruta.cs
+++ Ruta.cs
@@ -1,12 +1,27 @@
-internal class Ruta {
+internal class Ruta
+{
+
+    private string fechaHoraDeSalida;
+    private Estacion estacionDePartida;
+    private string fechaHoraDeLlegada;
+    private Estacion estacionDeLlegada;
+    private string idRuta;
+
+    public Estacion EstacionDePartida { get => estacionDePartida; set => estacionDePartida = value; }
+    public Estacion EstacionDeLlegada { get => estacionDeLlegada; set => estacionDeLlegada = value; }
+    public string FechaHoraDeSalida { get => fechaHoraDeSalida; set => idRuta = value; }
+    public string FechaHoraDeLlegada { get => fechaHoraDeLlegada; set => fechaHoraDeLlegada = value; }
+    public string IdRuta { get => idRuta; set => idRuta = value; }
 
 
+    public Ruta(string fechaHoraDeSalida, string fechaHoraDeLlegada, string idRuta)
+    {
 
+        FechaHoraDeSalida = fechaHoraDeSalida;
+        FechaHoraDeLlegada = fechaHoraDeLlegada;
+        IdRuta = idRuta;
 
-
-
-
-
+    }
 
 
 
