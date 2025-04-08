94a341fa9c86b098d38e62ca89d755ffa8e83a29
xocamacho <xommers.camacho@upb.edu.co>
2025-04-08 14:44:44 -0500

    Clases_Trenes_Vagones

--- Getion Rutas/Administrador.cs
+++ Getion Rutas/Administrador.cs
@@ -1,13 +1,45 @@
 internal class Administrador {
+   private string usuarioAdmin;
+   private string contrasenaAdmin;
+   private string idAdmin;
+   private bool txEnProgreso;
+   private Pasajero usuarioTx;
+
+   public string UsuarioAdmin { get => usuarioAdmin; set => usuarioAdmin = value; }
+   public string ContrasenaAdmin { get => contrasenaAdmin; set => contrasenaAdmin = value; }
+   public string IdAdmin { get => idAdmin; set => idAdmin = value; }
+   public bool TxEnProgreso { get => txEnProgreso; set => txEnProgreso = value; }
+   public Pasajero UsuarioTx { get => usuarioTx; set => usuarioTx = value; }
 
 
+   public Administrador(string usuarioAdmin, string contrasenaAdmin, string idAdmin)
+   {
+       UsuarioAdmin = usuarioAdmin;
+       ContrasenaAdmin = contrasenaAdmin;
+       IdAdmin = idAdmin;
+       TxEnProgreso = false;
+       UsuarioTx = null;
+   }
 
 
-
-
-
-
-
-
-
+   public bool IniciarSesion(string usuario_admin, string contrasena_admin)
+   {
+       if (this.UsuarioAdmin == usuario_admin && this.ContrasenaAdmin == contrasena_admin)
+       {
+           this.TxEnProgreso = true;
+           Console.WriteLine($"Bienvenido, administrador {this.UsuarioAdmin}. Has iniciado sesión correctamente.");
+           return true;
+       }
+       return false;
+   }
 }
+
+
+
+
+
+
+
+
+
+
--- Getion Rutas/Estacion.cs
+++ Getion Rutas/Estacion.cs
@@ -1,5 +1,23 @@
 internal class Estacion {
+ private String[] nombreEstacion;
+ private MaquinasDeVentas maquinaDeVenta;
 
+ public Estacion(String nombreEstacion)
+ {
+     this.nombreEstacion = new String[] { nombreEstacion };
+     this.maquinaDeVenta = new MaquinasDeVentas();
+ }
+
+ public String MostrarOrdenDeAbordaje()
+ {
+     System.out.println("MostrarOrdenDeAbordaje: Funcionalidad por hacer");
+     return "Funcionalidad por hacer";
+ }
+
+ public void cantidadDePasajeros()
+ {
+     System.out.println("cantidadDePasajeros: Funcionalidad por hacer");
+ }
 
 
 
