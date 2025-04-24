ba0d49442135dbc1324f7058d2580939c51eec25
xocamacho <xommers.camacho@upb.edu.co>
2025-04-23 19:17:00 -0500

    Clases_Trenes_Vagones

--- Gestion Trenes y Vagones/Empleado.cs
+++ Gestion Trenes y Vagones/Empleado.cs
@@ -1,9 +1,65 @@
 internal class Empleado {
+ {
+    private string UsuarioEmpleado;
+    private string ContrasenaEmpleado;
+    private string IdEmpleado;
+    private bool TxEnProgreso;
+    private Empleado UsuarioTx;
+    private string Nombre;
+    private string Apellido;
 
+    public Empleado(string usuarioEmpleado, string contrasenaEmpleado, string idEmpleado, string nombre, string apellido)
+    {
+        this.UsuarioEmpleado = usuarioEmpleado;
+        this.ContrasenaEmpleado = contrasenaEmpleado;
+        this.IdEmpleado = idEmpleado;
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.TxEnProgreso = false;
+        this.UsuarioTx = null;
+    }
 
+    public string GetUsuarioEmpleado()
+    {
+        return UsuarioEmpleado;
+    }
 
+    public void SetUsuarioEmpleado(string usuarioEmpleado)
+    {
+        this.UsuarioEmpleado = usuarioEmpleado;
+    }
 
+    public string GetContrasenaEmpleado()
+    {
+        return ContrasenaEmpleado;
+    }
 
+    public void SetContrasenaEmpleado(string contrasenaEmpleado)
+    {
+        this.ContrasenaEmpleado = contrasenaEmpleado;
+    }
 
+    public string GetIdEmpleado()
+    {
+        return IdEmpleado;
+    }
 
+    public void SetIdEmpleado(string idEmpleado)
+    {
+        this.IdEmpleado = idEmpleado;
+    }
+
+    public bool IniciarSesion(string usuarioEmpleado, string contrasenaEmpleado)
+    {
+        if (this.UsuarioEmpleado == usuarioEmpleado && this.ContrasenaEmpleado == contrasenaEmpleado)
+        {
+            this.UsuarioTx = this;
+            this.TxEnProgreso = true;
+            Console.WriteLine($"Bienvenido, {this.Nombre}, {this.Apellido}. Has iniciado sesión correctamente.");
+            return true;
+        }
+        return false;
+    }
+ }
 }
+
